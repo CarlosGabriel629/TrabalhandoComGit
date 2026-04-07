ffd8ae78364d963769efabcfd5bb60331cab9316
CarlosGabriel629 <carlos.g.santos@edu.seani.br>
2026-04-07 16:01:38 -0300

    feat: subindo Ex01.js

---
+++ Ex01.js
@@ -0,0 +1,10 @@
+// 
+const readline = require('readline-sync')
+let nome = readline.question('informe seu nome')
+let curso = readline.question('informe seu curso')
+let idade = readline.questionInt('informe sua idade')
+
+console.log('=========== SITES DE CADASTRO ============')
+console.log (`informe seu nome ${nome}`)
+console.log (`informe o nome do curso ${curso}`)
+console.log (`informe sua idade ${idade}`)
