3f794eb712033d1784f3e6f69323225d5e01aabc
CarlosGabriel629 <carlos.g.santos@edu.seani.br>
2026-04-07 15:09:31 -0300

    feat: estruturacondional.js

---
+++ estruturaCondicional.js
@@ -0,0 +1,24 @@
+// let sinal = 'vermelho'
+
+// if (sinal== 'verde'){
+// console.log ('pode atravesar')
+// }else{
+//     console.log('NAO PODE ATRAVESAR')
+// }
+
+// 
+// 
+// var nota = 10
+// if(nota >= 7){
+// console.log('Aprovado')
+// }else{
+//     console.log('Reprovado')
+// }
+
+let numero= 34
+if(numero % 2 == 0){
+    console.log('O numero e par')
+
+}else{
+    console.log('O numero e impar')
+}
